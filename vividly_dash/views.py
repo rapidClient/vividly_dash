ec880a2f26be4f5195aa6d35287105bf1a2899c5
[chideracollins] <[siralexcollins@gmail.com]>
2024-10-23 13:13:52 +0100

    corrected code look up in access token view

--- vividly_dash/views.py
+++ vividly_dash/views.py
@@ -8,7 +8,7 @@ def access_token(server: Flask) -> None:
     @server.route("/access_token")
     def redirect():
         if "code" in request.args.keys():
-            auth_code = request.args.get("access_token")
+            auth_code = request.args.get("code")
             endpoint = META_ACCESS_TOKEN_URL + f"&code={auth_code}"
             response = requests.get(endpoint)
             return response.json()
